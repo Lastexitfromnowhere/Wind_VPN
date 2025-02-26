69e49311992c6edd353e3f43a09079808d4f1053
Lastexitfromnowhere <core.bardage@gmail.com>
2025-02-26 17:47:39 +0100

    Fix: Update network stats handler format

--- api/networkStats.js
+++ api/networkStats.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const getNetworkStats = require('./networkStats');
 
-router.get('/network-stats', (req, res) => {
-  const stats = {
-    totalNodes: 1247,
-    activeNodes: 892,
-    totalBandwidth: 42.5,
-    averageUptime: 98.5,
-    networkHealth: 'healthy'
-  };
-  
-  res.json(stats);
-});
+router.get('/network-stats', getNetworkStats);
 
 module.exports = router;
